5fc1fb872c3b5d2fd068cbec099bbf843cd53cb8
Khosravi Mahdi <mahdi.khosravi@scania.com>
2023-10-30 12:57:07 +0100

    Products models were migrated.

--- digikala/settings.py
+++ digikala/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    
+    'products'
 ]
 
 MIDDLEWARE = [
@@ -118,8 +121,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # media
-MEDIA_ROOT = os.path.join('BASE_DIR', 'media/')
-MEDIA_URL = '/media/
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 
 
--- products/models.py
+++ products/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 class Category(models.Model):
-    parent= models.ForeignKey('self', verbose_name='parent',blank=True, null=True,on_delete=models.cascade.CASCADE)
+    parent= models.ForeignKey('self', verbose_name='parent',blank=True, null=True,on_delete=models.CASCADE)
     title = models.CharField(_('title') ,max_length=50)
     description = models.TextField(_('description') ,blank=True)
     avatar=models.ImageField(_('avatar'),blank=True,upload_to='categories')
@@ -23,7 +23,7 @@ class Product(models.Model):
     description = models.TextField(_('description'), blank=True)
     avatar = models.ImageField(_('avatar'), blank=True, upload_to='products/')
     is_enable = models.BooleanField(_('is enable'), default=True)
-    categories = models.ManyToManyField(_('Category'), verbose_name=_('categories'), blank=True) # many products can be connected to many category
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True) # many products can be connected to many category
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
 
